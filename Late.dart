904de0b354f6884a6f52b8d08cc4f55458ab1421
Anirbjb <146140599+Anirbjb@users.noreply.github.com>
2023-10-04 20:10:23 +0700

    Create Late.dart

---
+++ Late.dart
@@ -0,0 +1,16 @@
+void main(){
+  late var value = getValue();
+    print('display value');
+  print("value");
+}
+
+ getValue(){
+  print ('getvalue dipanggil');
+  return 'eko kurniawan';
+  
+}
+
+  
+  
+
+ 
