4c67988512550c8a6243f5e09fc7d986f6c690c8
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-05 05:30:19 +0700

    Create Number.dart

---
+++ Number.dart
@@ -0,0 +1,11 @@
+void main() {
+   // Mendeklarasikan variable int
+   int num1 = 10;             
+     
+   // mendeklarasikan variable double 
+   double num2 = 10.50;  
+
+   // melakukan print value dari num1 dan num2
+   print(num1);
+   print(num2);
+}
