d68382e88a5184c7bbb2850c65da24038823a04d
Anirbjb <146140599+Anirbjb@users.noreply.github.com>
2023-10-04 20:19:55 +0700

    Create Komentar.dart

---
+++ Komentar.dart
@@ -0,0 +1,11 @@
+///ini adalah dokumentasi
+///yang biasanya akan digenerate menjadi dart doc
+void main(){
+  //this is variable
+  var name = 'Anir';
+  /**
+   * ini adalah komentar 
+   * lebih dari satu baris
+   */
+  print (name);
+}
