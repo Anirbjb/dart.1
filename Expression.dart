a7064aa9c51655c3ab2e2197bcf66d77713bda16
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-05 06:26:05 +0700

    Create Expression.dart

---
+++ Expression.dart
@@ -0,0 +1,10 @@
+void main(){
+  String firstname ='Anir';
+  
+  String lastname = "William";
+  
+  // huruf kapital penulisan "S" pada kalimat (String)
+  var fullname = '$firstname $lastname';
+ 
+  print(fullname);
+}
