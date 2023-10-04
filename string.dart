3b23ad0f3704aef7bc461cc42699d67308b308c1
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-05 06:17:55 +0700

    Create string.dart

---
+++ string.dart
@@ -0,0 +1,10 @@
+void main(){
+String firstname = 'Anir';
+String lastname = "Willian";
+
+
+//Tanpa String Interpolation
+print(firstname);
+print(lastname);
+print("Nama lengkap saya"+firstname+lastname+"");
+}
