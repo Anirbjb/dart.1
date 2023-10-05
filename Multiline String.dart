645b54207a59fd53d535cd3463771eb1fc17eaca
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-05 07:41:49 +0700

    Create Multiline String.dart

---
+++ Multiline String.dart
@@ -0,0 +1,6 @@
+void main(){
+  var longString = '''String ini sangat panjang sehingga sulit dibuat dalam satu baris program''';
+  var longString1 = '''dengan cara ini mungkin lebih mempermudah penulisan paragraf yang panjang''';
+  print(longString);
+  print(longString1);
+}
