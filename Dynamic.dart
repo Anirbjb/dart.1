891584851a5912b1b26525ee918a716197900be0
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-05 07:47:37 +0700

    Create Dynamic.dart

---
+++ Dynamic.dart
@@ -0,0 +1,10 @@
+void main(){
+  dynamic variable = 100;
+  print (variable);
+  
+  variable=true; //huruf 't' pada true tidak bisa ditulis kapital
+  print(variable);
+  
+  variable ='Anir';
+  print(variable);
+}
