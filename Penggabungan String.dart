149b8d0ec1bc002f8ca5615467b4a4a87a25b258
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-05 07:33:51 +0700

    Create Penggabungan String.dart

---
+++ Penggabungan String.dart
@@ -0,0 +1,12 @@
+void main(){
+  String firstname='';
+  String lastname= "";
+  
+  //Penggabungan String
+  
+  var name1= firstname + lastname;
+  var name2= 'Anir' 'William';
+  
+  print(name1);
+  print(name2);
+}
