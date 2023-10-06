da852f7a74a3f877842733ec7b7a5ecdc025be68
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-06 14:30:09 +0700

    Create operator type test.dart

---
+++ operator type test.dart
@@ -0,0 +1,18 @@
+void main() {
+  dynamic variable= 100;
+  
+  var varibleInt = variable as int;
+  
+  var isInt = variable is int;
+  
+  var isnotboolean = variable is! bool;
+  
+  
+  
+  print(varibleInt);
+  print(isInt);
+  print(isnotboolean);
+ 
+}
+
+
