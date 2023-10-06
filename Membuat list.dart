57a1a34650002d981425e12a73be30cff2656cb4
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-06 14:37:34 +0700

    Create Membuat list.dart

---
+++ Membuat list.dart
@@ -0,0 +1,13 @@
+void main() {
+  //create List of int
+  List<int> listInt = [];
+  
+  //creat list of string
+  var listString= <String>[];
+  
+  print(listInt);
+  print(listString);
+  
+}
+
+
