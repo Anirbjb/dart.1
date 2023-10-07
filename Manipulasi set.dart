7a5721e24ffe3347ce693b53a1ebb92e29251f50
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-07 16:13:33 +0700

    Create Manipulasi set.dart

---
+++ Manipulasi set.dart
@@ -0,0 +1,14 @@
+void main(){
+ var names = <String>[];
+ 
+  names.add('Anir');
+  names.add('William');
+  names.add('STMIK');
+  
+  
+  print (names);
+  
+  names.remove('Anir');
+  print (names);
+   
+  }
