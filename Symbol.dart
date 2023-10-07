b524ed359281410e953f92c270ff4641c4455f5e
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-07 16:45:20 +0700

    Create Symbol.dart

---
+++ Symbol.dart
@@ -0,0 +1,13 @@
+void main(){
+Symbol symbol1= Symbol('Anir');
+ var symbol2= #belajar;
+ var symbol3= #dan_bekerja_keras;
+  
+  
+ print(symbol1);
+ print(symbol2);
+ print(symbol3);
+ 
+  
+
+  }
