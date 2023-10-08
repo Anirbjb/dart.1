5cac7b9a99933f9a8b92df2d907f5bc55469df95
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-08 20:15:56 +0700

    Create Function parameter.dart

---
+++ Function parameter.dart
@@ -0,0 +1,9 @@
+void printInfo({String? name, String? gender}) {
+  print("Hello $name your gender is $gender.");
+}
+
+void main() {
+  // you can pass values in any order in named parameters.
+  printInfo(gender: "Anir", name: "William");
+ 
+  }
