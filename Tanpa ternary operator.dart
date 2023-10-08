99e4f875d47b0270c911eedb920e3404449be4c8
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-08 09:14:06 +0700

    Create Tanpa ternary operator.dart

---
+++ Tanpa ternary operator.dart
@@ -0,0 +1,12 @@
+void main(){
+  var nilai =75;
+  String ucapan;
+  
+  if (nilai >=75 && nilai<=75){
+    ucapan=' nilai anda memuaskan.Selamat anda lulus';
+  }else{
+    ucapan= ' nilai anda sangat rendah.Tidak Lulus';
+  }
+  print(ucapan);
+ }
+
