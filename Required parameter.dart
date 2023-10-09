8160be2effe01cc9d89861666e52701942ede7ac
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-09 19:23:41 +0700

    Create Required parameter.dart

---
+++ Required parameter.dart
@@ -0,0 +1,8 @@
+void main() {
+  favCities('Munich', 'Lahore', 'Lisbon');
+}
+void favCities(String name1, String name2, String name3) {
+  print('First fav city name is $name1');
+  print('Second fav city name is $name2');
+  print('Third fav city name is $name3');
+}
