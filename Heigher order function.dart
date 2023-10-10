35c3af472e08aa8377e34b35407e44be17ffb8f1
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-10 09:45:09 +0700

    Create Heigher order function.dart

---
+++ Heigher order function.dart
@@ -0,0 +1,16 @@
+void getSum(List<int> numbers,Function result){
+  var sum= 0;
+  for(var i=0; i<numbers.length; i++){
+    sum+=numbers[i];
+  }
+  result(sum);
+}
+
+main(){
+  var nums =[2,2,4,4,];
+  Function f1 =( int sum){
+    print('sum is;');
+    print(sum);
+  };
+  getSum(nums,f1);
+}
